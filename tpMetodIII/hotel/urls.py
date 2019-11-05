e0a0eae2381d75ec7341fe5832f5d7cfb1b28157
Your Name <you@example.com>
2019-11-05 12:03:51 -0300

    mejorado un poco el diseño

--- tpMetodIII/hotel/urls.py
+++ tpMetodIII/hotel/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('ownerships/<int:ownership_id>', ownership_details, name="ownership_details")
 ]
--- tpMetodIII/hotel/views.py
+++ tpMetodIII/hotel/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from hotel.models import *
 
 # Create your views here.
 
 
 def index(request):
-    cities = get_list_or_404(City) # me traigo todas las ciudades y si no hay ninguna lanzo error 404
     ownerships = Ownership.objects.all() # me traigo todas las propiedades y no importa si no hay ninguna
     context = {
-        'cities': cities,
         'ownerships': ownerships
     }
 
     return render(request, 'hotel/home.html', context)
+
+
+def ownership_details(request, ownership_id):
+
+    ownership = get_object_or_404(Ownership, pk=ownership_id)
+    context = {
+        'ownership':ownership
+    }
+    return render(request, 'hotel/ownership_details.html', context);
